603c96981aac11c960cfc3775479dea232b7a036
Aashish <aashishkafle9824@gmail.com>
2023-12-07 12:08:07 +0545

    Exercise 1 updated

---
+++ Exercise1/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Car
+    {
+        //Properties
+        private string Brand;
+        private int Year;
+        private float Price;
+
+        //Constructor
+        public Car(string brand, int year, float price)
+        {
+            Brand = brand;
+            Year = year;
+            Price = price;
+        }
+
+        //Creating method that prints the details of car
+        public void Output()
+        {
+            Console.WriteLine($"Brand of car is {Brand}, year of launch is {Year} and price is {Price}");
+        }
+    }
+    
+}
---
+++ Exercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating instance of Car
+            Car car= new Car("Lamborghini",2022,120000);
+
+            //Calling the Output method
+            car.Output();
+        }
+    }
+}
